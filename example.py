7ff54a16396d866e65f6c284167d3c8aaedba62f
JetsonHacks <jim@jetsonhacks.com>
2025-03-21 17:39:35 -0700

    Update example.py

--- example.py
+++ example.py
@@ -1,6 +1,6 @@
-#! /bin/python
+#!/usr/bin/python3
 # Expected result: 3.3 volts using a volt meter on pin 7 
-import RPi.GPIO as GPIO
+import Jetson.GPIO as GPIO
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(7, GPIO.OUT)
